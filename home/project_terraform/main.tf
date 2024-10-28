fd2dbb6683a6c7e8833d3bbf9133f62a17c7ff6d
switkowski81 <34235104+switkowski81@users.noreply.github.com>
2024-10-28 16:08:06 +0100

    Update main.tf

--- home/project_terraform/main.tf
+++ home/project_terraform/main.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 }
@@ -20,28 +30,6 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
-
-module "ec2" {
-  source          = "./modules/ec2"
-  instance_count  = var.instance_count
-  ami_id          = var.ami_id
-  instance_type   = var.instance_type
-  instance_name   = var.instance_name
-  subnet_id       = aws_subnet.public.id
-  vpc_id          = aws_vpc.main.id 
-}
-
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  map_public_ip_on_launch = true  
-}
-
 resource "aws_security_group" "ec2_sg" {
   vpc_id = aws_vpc.main.id
 
@@ -51,13 +39,31 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+module "ec2" {
+  source          = "./modules/ec2"
+  instance_count  = var.instance_count
+  ami_id          = var.ami_id
+  instance_type   = var.instance_type
+  instance_name   = var.instance_name
+  subnet_id       = aws_subnet.public.id
+  vpc_id          = aws_vpc.main.id
+  security_group_ids = [aws_security_group.ec2_sg.id]  # Pass the security group ID
 }
 
 resource "aws_launch_configuration" "ec2_launch_config" {
   image_id        = var.ami_id
   instance_type   = var.instance_type
   security_groups = [aws_security_group.ec2_sg.id]
-  key_name        = "project_key"  
+  key_name        = "project_key"
 
   lifecycle {
     create_before_destroy = true
