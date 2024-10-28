8a740d85d498a86e56566c4b389a31bf74b8a185
switkowski81 <34235104+switkowski81@users.noreply.github.com>
2024-10-28 10:37:54 +0100

    Update run_ansible.sh

--- home/project_ansible/run_ansible.sh
+++ home/project_ansible/run_ansible.sh
@@ -2,3 +2,6 @@
 
 # Aktualizacja inventory
 /home/project_terraform/modules/ec2/inventory.sh
+
+# Uruchomienie playbooka Ansible na wszystkich instancjach
+ansible-playbook -i /home/project_ansible/inventory /home/project_ansible/playbook.yml
