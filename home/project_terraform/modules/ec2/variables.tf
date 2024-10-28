112d6ded2050abb0c884c2a9b0a9235b256404e6
switkowski81 <34235104+switkowski81@users.noreply.github.com>
2024-10-28 15:07:10 +0100

    Update variables.tf

--- home/project_terraform/modules/ec2/variables.tf
+++ home/project_terraform/modules/ec2/variables.tf
@@ -7,3 +7,13 @@ variable "instance_name" {
   type        = string
   default     = "default-ec2-instance"  
 }
+
+variable "subnet_id" {
+  description = "ID podsieci, do której zostaną przypisane instancje EC2"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
