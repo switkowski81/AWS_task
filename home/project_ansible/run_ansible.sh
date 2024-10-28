9eb89c74dec5638118e3e2b60073385a612c6153
switkowski81 <34235104+switkowski81@users.noreply.github.com>
2024-10-28 14:28:43 +0100

    Delete home/project_ansible/run_ansible.sh

--- home/project_ansible/run_ansible.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Aktualizacja inventory
-/home/project_terraform/modules/ec2/inventory.sh
-
-# Uruchomienie playbooka Ansible na wszystkich instancjach
-ansible-playbook -i /home/project_ansible/inventory /home/project_ansible/playbook.yml
