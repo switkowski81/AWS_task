3cb66745a0a5849e6a451b7f5e6daf3535729f8a
switkowski81 <34235104+switkowski81@users.noreply.github.com>
2024-10-28 15:06:17 +0100

    Update main.tf

--- home/project_terraform/modules/ec2/main.tf
+++ home/project_terraform/modules/ec2/main.tf
@@ -1,14 +1,27 @@
-resource "aws_instance" "ec2_instance" {
-  count         = var.instance_count
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = "project_key"
-  security_groups = [aws_security_group.ec2_sg.name]
+
+resource "random_pet" "name" {
+  length = 2  # Można dostosować długość generowanej nazwy
 }
+
+resource "aws_instance" "ec2_instance" {
+  count                      = var.instance_count
+  ami                        = var.ami_id
+  instance_type              = var.instance_type
+  key_name                   = "project_key"
+  vpc_security_group_ids     = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = true
+  subnet_id                  = var.subnet_id
+
+  tags = {
+    Name = "${var.instance_name}-${random_pet.name.id}"
+  }
+}
+
 
 resource "aws_security_group" "ec2_sg" {
   name        = "${var.instance_name}-sg"
   description = "Security group for EC2 instances"
+  vpc_id      = var.vpc_id  
 
   ingress {
     from_port   = 22
