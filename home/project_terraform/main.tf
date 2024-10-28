826e4e1b34cb98d85c82322a9e934c8caadf22ee
switkowski81 <34235104+switkowski81@users.noreply.github.com>
2024-10-28 15:03:48 +0100

    Update main.tf

--- home/project_terraform/main.tf
+++ home/project_terraform/main.tf
@@ -2,13 +2,35 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+
 module "ec2" {
   source          = "./modules/ec2"
   instance_count  = var.instance_count
   ami_id          = var.ami_id
   instance_type   = var.instance_type
   instance_name   = var.instance_name
+  subnet_id       = aws_subnet.public.id
+  vpc_id          = aws_vpc.main.id  # Przekazanie ID VPC
 }
+
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
@@ -17,10 +39,12 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true  # Każda instancja w tej podsieci będzie miała publiczne IP
 }
 
 resource "aws_security_group" "ec2_sg" {
   vpc_id = aws_vpc.main.id
+
   ingress {
     from_port   = 22
     to_port     = 22
@@ -44,7 +68,7 @@ resource "aws_autoscaling_group" "ec2_asg" {
   launch_configuration = aws_launch_configuration.ec2_launch_config.id
   min_size             = 1
   max_size             = 5
-  desired_capacity     = 1
+  desired_capacity     = var.instance_count
   vpc_zone_identifier  = [aws_subnet.public.id]
   health_check_type    = "EC2"
   health_check_grace_period = 300
@@ -54,29 +78,4 @@ resource "aws_autoscaling_group" "ec2_asg" {
     value               = var.instance_name
     propagate_at_launch = true
   }
-}
-
-resource "aws_cloudwatch_metric_alarm" "cpu_alarm" {
-  alarm_name          = "high_cpu_alarm"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = "300"
-  statistic           = "Average"
-  threshold           = "90"
-  alarm_description   = "Alarm when CPU exceeds 90%"
-  actions_enabled     = true
-  alarm_actions       = [aws_autoscaling_policy.scale_up.arn]
-  dimensions = {
-    AutoScalingGroupName = aws_autoscaling_group.ec2_asg.name
-  }
-}
-
-resource "aws_autoscaling_policy" "scale_up" {
-  name                   = "scale_up_policy"
-  scaling_adjustment     = 1
-  adjustment_type        = "ChangeInCapacity"
-  cooldown               = 300
-  autoscaling_group_name = aws_autoscaling_group.ec2_asg.name
 }
