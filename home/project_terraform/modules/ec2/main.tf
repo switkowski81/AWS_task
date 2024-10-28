0ca592a32b607afa89d84035e27c1ef6b2cc15ba
switkowski81 <34235104+switkowski81@users.noreply.github.com>
2024-10-28 16:09:25 +0100

    Update main.tf

--- home/project_terraform/modules/ec2/main.tf
+++ home/project_terraform/modules/ec2/main.tf
@@ -16,24 +16,3 @@ resource "aws_instance" "ec2_instance" {
     Name = "${var.instance_name}-${random_pet.name.id}"
   }
 }
-
-
-resource "aws_security_group" "ec2_sg" {
-  name        = "${var.instance_name}-sg"
-  description = "Security group for EC2 instances"
-  vpc_id      = var.vpc_id  
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
